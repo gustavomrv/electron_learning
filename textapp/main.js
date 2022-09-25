a8509bf5f99f6ca0406773c390ac48fda9747e20
Gustavo Moura Barros <gustavomourago@gmail.com>
2022-09-25 11:03:20 -0300

    testes com flappy bird

--- textapp/main.js
+++ textapp/main.js
@@ -1,8 +1,16 @@
 const electron = require('electron')
-const {app, BrowserWindow} = electron
+const {app, BrowserWindow, ipcMain} = electron
 
 app.on('ready', () =>{
-    let win = new BrowserWindow({})
+    let win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
     win.loadFile('index.html')
 })
 
+ipcMain.on('save', (event, text)=>{
+    // saving the text in a file
+    console.log(text)
+})
